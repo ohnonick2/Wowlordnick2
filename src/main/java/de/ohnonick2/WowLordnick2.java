c72f7a6779964f6e6174b8ea62ec3e87d35db4d9
ohnonick2 <lukasmeyer317@gmail.com>
2024-01-01 12:53:13 +0100

    Initial

---
+++ src/main/java/de/ohnonick2/WowLordnick2.java
@@ -0,0 +1,20 @@
+package de.ohnonick2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WowLordnick2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
